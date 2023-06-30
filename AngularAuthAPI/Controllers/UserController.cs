d28d0ef24b57133c8568041076a905570c5344e5
jorgeturbi <jturbi@syschar.com>
2023-06-30 00:42:31 -0400

    Create JWT

--- AngularAuthAPI/Controllers/UserController.cs
+++ AngularAuthAPI/Controllers/UserController.cs
@@ -1,10 +1,11 @@
 ﻿using AngularAuthAPI.Context;
 using AngularAuthAPI.Helpers;
 using AngularAuthAPI.Models;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -30,9 +31,10 @@ namespace AngularAuthAPI.Controllers
 
             if(!PasswordHasher.VerifyPassword(userObj.Password,user.Password))
                 return BadRequest("Password is Incorrect");
-
+            user.Token = CreateJwt(user);
             return Ok(new
             {
+                Toke=user.Token,
                 Message = "Login Success!"
             });
         }
@@ -81,7 +83,34 @@ namespace AngularAuthAPI.Controllers
             return sb.ToString();
         }
 
+       private string CreateJwt(User user)
+        {
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("veryverysecret....");
+            var identity = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Role,user.Role),
+                new Claim(ClaimTypes.Name,$"{user.FirstName}{user.LastName}")
 
+            });
+            var credentials = new SigningCredentials(new SymmetricSecurityKey(key),SecurityAlgorithms.HmacSha256);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = identity,
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = credentials,
+            };
+            var token=jwtTokenHandler.CreateToken(tokenDescriptor);
+            return jwtTokenHandler.WriteToken(token);
+        }
+
+
+
+        [HttpGet]
+        public async Task<ActionResult<User>> GetAllUser()
+        {
+            return Ok(await _context.Users.ToListAsync());
+        }
     }
 
 
--- AngularAuthAPI/Program.cs
+++ AngularAuthAPI/Program.cs
@@ -1,5 +1,9 @@
 using AngularAuthAPI.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,6 +29,24 @@ builder.Services.AddDbContext<AppDbContext>(Options =>
     Options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnStr"));
 });
 
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("veryverysecret....")),
+        ValidateAudience = false,
+        ValidateIssuer=false,
+
+};
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -37,6 +59,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseCors("MyPolicy");
 app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
